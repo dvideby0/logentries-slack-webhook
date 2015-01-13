703407b23bad1c8f511e44ee5fc10e139a06a36c
dvideby0 <rbrookfield@352inc.com>
2015-01-13 07:22:38 -0500

    added response ends

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var moment = require('moment');
 
 app.post('/log/:service', function(req, res) {
   if (req.params.service == 'logentries') {
-    console.log(req.body);
+    console.log(JSON.parse(req.body.payload));
     var request = require('request');
     var data = {
       text: req.body.alert.name,
